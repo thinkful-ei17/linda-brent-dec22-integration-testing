f9f8da17ba121d951aa0b7e89316018438b61702
Linda Foo <linda.foo.dev@gmail.com>
2017-12-22 12:47:02 -0800

    added put and delete

--- test/test-server.js
+++ test/test-server.js
@@ -194,5 +194,38 @@ describe('Recipe list', function(){
       });
   });
 
+  it('should update items on PUT', function() {
+    const updateData = {
+      name: 'ice cream',
+      ingredients: ['ice', 'cream', 'vanilla']
+    };
+
+    return chai.request(app)
+      .get('/recipes')
+      .then(function(res) {
+        updateData.id = res.body[0].id;
+
+        return chai.request(app)
+          .put(`/recipes/${updateData.id}`)
+          .send(updateData);
+      })
+
+      .then(function(res) {
+        res.should.have.status(204);
+      });
+  });
+
+  it('should delete items on DELETE', function() {
+    return chai.request(app)
+      .get('/recipes')
+      .then(function(res) {
+        return chai.request(app)
+          .delete(`/recipes/${res.body[0].id}`);
+      })
+      .then(function(res) {
+        res.should.have.status(204);
+      });
+  });
 });
+
 
